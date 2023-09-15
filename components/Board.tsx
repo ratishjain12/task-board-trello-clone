bddabfa85d24930ca576e90662d8688d488a6b74
ratishjain <ratishjain6@gmail.com>
2023-09-15 20:16:43 +0530

    appwrite integrated

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-
+import { useEffect } from "react";
+import { useBoardStore } from "@/store/BoardStore";
 function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div>{/* Render columns */}</div>}
-      </Droppable>
-    </DragDropContext>
+    <h1>Hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/* Render columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 }
 export default Board;
