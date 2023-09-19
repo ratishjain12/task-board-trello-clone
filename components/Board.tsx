d85530bf0db25fd3d5ca1afd809d6a30c954eda3
ratishjain <ratishjain6@gmail.com>
2023-09-20 00:32:42 +0530

    added card draggin

--- components/Board.tsx
+++ components/Board.tsx
@@ -27,9 +27,56 @@ function Board() {
         ...board,
         columns: rearrangedCol,
       });
-    }
+    } else {
+      //handle card
+      const columns = Array.from(board.columns);
+      const startColIndex = columns[Number(source.droppableId)];
+      const endColIndex = columns[Number(destination.droppableId)];
 
-    //handle card
+      const startCol: Column = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+      };
+      const endCol: Column = {
+        id: endColIndex[0],
+        todos: endColIndex[1].todos,
+      };
+
+      if (!startCol || !endCol) return;
+
+      if (source.index === destination.index && startCol === endCol) return;
+
+      const newTodos = startCol.todos;
+      const [todoMoved] = newTodos.splice(source.index, 0);
+      if (startCol.id === endCol.id) {
+        // same col drag
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+
+        setBoardState({ ...board, columns: newColumns });
+      } else {
+        //drag to another column
+        const finishTodos = Array.from(endCol.todos);
+        finishTodos.splice(destination.index, 0, todoMoved);
+        const newCol = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+        newColumns.set(endCol.id, {
+          id: endCol.id,
+          todos: finishTodos,
+        });
+
+        setBoardState({ ...board, columns: newColumns });
+      }
+    }
   };
   return (
     <div className=" w-[100%] flex  justify-center items-center ">
